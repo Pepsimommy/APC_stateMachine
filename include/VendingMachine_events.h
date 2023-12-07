362c8ce02b226a5ccbff0cad2a7ee790f0539a75
Pepsimommy <alexandrina3002@gmail.com>
2023-12-07 16:09:16 +0100

    bag pula

---
+++ include/VendingMachine_events.h
@@ -0,0 +1,14 @@
+//
+// Created by Gusita on 12/7/2023.
+//
+
+#ifndef PROJECT_VENDINGMACHINE_EVENTS_H
+#define PROJECT_VENDINGMACHINE_EVENTS_H
+enum class VendingMachine_events{
+    INSERT_MONEY,
+    MAKE_SELECTION,
+    DISPENSE_ITEM,
+    REFUND_CHANGE,
+    OUT_OF_STOCK
+};
+#endif //PROJECT_VENDINGMACHINE_EVENTS_H
